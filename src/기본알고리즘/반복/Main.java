480c60a1fdd8813250f9761aa0b69a9e8311cc37
dev_k9 <dev_k9@bizbee.co.kr>
2026-01-29 15:02:59 +0900

    1.기본 알고리즘 - (1-2)반복

--- src/기본알고리즘/반복/Main.java
+++ src/기본알고리즘/반복/Main.java
@@ -1,4 +1,25 @@
 package 기본알고리즘.반복;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("1부터 n까지의 합을 구합니다.");
+        System.out.print("n = ");
+        int n = sc.nextInt();
+
+        System.out.println("Sum = " + sum(n));
+
+    }
+
+    public static int sum(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        return sum;
+    }
 }
