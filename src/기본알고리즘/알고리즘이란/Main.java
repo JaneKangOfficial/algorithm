4244ddc1738245364e4191ce8c0942e809cab8c7
dev_k9 <dev_k9@bizbee.co.kr>
2026-01-29 15:02:48 +0900

    1.기본 알고리즘 - (1-1)알고리즘이란

--- src/기본알고리즘/알고리즘이란/Main.java
+++ src/기본알고리즘/알고리즘이란/Main.java
@@ -1,4 +1,75 @@
 package 기본알고리즘.알고리즘이란;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("a = ");
+        int a = sc.nextInt();
+
+        System.out.print("b = ");
+        int b = sc.nextInt();
+
+        System.out.print("c = ");
+        int c = sc.nextInt();
+
+        System.out.print("d = ");
+        int d = sc.nextInt();
+
+        System.out.println("max4 = " + max4(a, b, c, d));
+        System.out.println("min3 = " + min3(a, b, c));
+        System.out.println("min4 = " + min4(a, b, c, d));
+        System.out.println("med3 = " + med3(a, b, c));
+    }
+
+    public static int max4(int a, int b, int c, int d) {
+        int max = a;
+
+        if (b > max) max = b;
+        if (c > max) max = c;
+        if (d > max) max = d;
+
+        return max;
+    }
+
+    public static int min3(int a, int b, int c) {
+        int min = a;
+
+        if (b < min) min = b;
+        if (c < min) min = c;
+
+        return min;
+    }
+
+    public static int min4(int a, int b, int c, int d) {
+        int min = a;
+
+        if (b < min) min = b;
+        if (c < min) min = c;
+        if (d < min) min = d;
+
+        return min;
+    }
+
+    public static int med3(int a, int b, int c) {
+        if (a >= b) {
+            if (b >= c) {
+                return b;
+            } else if (a >= c) {
+                return c;
+            } else {
+                return a;
+            }
+        } else if (a > c) {
+            return a;
+        } else if (b > c) {
+            return c;
+        } else {
+            return b;
+        }
+    }
+
 }
